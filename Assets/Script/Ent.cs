a5fc44d9d55e7efb5aa744d4ff775e80ca91dfc1
Carles Vallve <snaptothegrid@gmail.com>
2014-11-29 15:16:38 +0900

    First commit

---
+++ Assets/Script/Ent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ent : MonoBehaviour {
+
+	private Grid grid;
+
+	public void init (Grid grid, Vector3 pos) {
+		this.grid = grid;
+
+		transform.parent = grid.transform;
+		transform.localPosition = pos;
+
+		grid.cam.target = transform;
+	}
+}
---
+++ Assets/Script/Tile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	private Grid grid;
+
+
+	public void init (Grid grid, Vector3 pos) {
+		this.grid = grid;
+
+		transform.parent = grid.transform;
+		transform.localPosition = pos;
+	}
+}
